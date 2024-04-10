ebb1d1d1c8b168b11e1e0cc84a29720b9cd5af08
Yaritza Miranda <yaritza1095@gmail.com>
2024-04-10 19:19:57 -0400

    Test Automation University exercises

---
+++ Pytestexercises/chaptersix/tests/conftest.py
@@ -0,0 +1,52 @@
+import pytest
+
+from Pytestexercises.chaptersix.stuff.accum import Accumulator
+'''
+There are a few advanced tricks you can do with fixtures as well. 
+If you want to share fixtures between multiple test modules, you can move it to a module in the tests directory named conftest.py. 
+conftest.py modules share test code for pytest. The name of the module is important. 
+Pytest will automatically pick up any fixtures here.
+'''
+@pytest.fixture
+def accum():
+  return Accumulator()
+
+@pytest.fixture
+def accum2():
+  return Accumulator()
+
+def test_accumulator_init(accum, accum2):
+  assert accum.count == 0
+'''
+ fixtures can handle both setup _and _cleanup. 
+ If you use a yield statement instead of a return statement in a fixture, 
+ the fixture function becomes something known in Python as a generator
+ Basically, everything before the fixture's yield statement will be the "setup" steps, 
+ and everything after the fixture's yield statement will be the "cleanup" steps. 
+ The fixture will resume execution after the yield statement when the test case function completes, regardless of whether or not the test passed.
+
+You can also change the scope of the fixture, or when the fixture is run. 
+By default, the scope is set to "function", meaning that the fixture will run once for each function that needs it. 
+However, if you change the scope to "session", then the fixture runs only one time for the entire test suite.
+
+If multiple tests use the fixture, then the fixture will run only for the first test. 
+pytest will then store its return value and simply inject the return value into each subsequent test that needs it.
+
+Session scope would not be appropriate for these Accumulator tests, but it would be appropriate for a fixture that needs to read data from an external file. 
+Other scope levels include "class", "module", and "package".
+
+Finally, pytest provides several fixtures out of the box:
+
+monkeypatch can be used for modifying classes, functions, and other objects
+request provides test case metadata
+tmpdir and tmp_path provide temporary directories
+pytest plugins may also provide additional fixtures.
+'''
+@pytest.fixture
+def accum():
+    yield Accumulator()
+    print('DONE-ZO')
+
+@pytest.fixture
+def accum(scope="session"):
+  return Accumulator()
---
+++ Pytestexercises/chaptersix/tests/test_accum.py
@@ -0,0 +1,54 @@
+import pytest
+
+from Pytestexercises.chaptersix.stuff.accum import Accumulator
+'''
+Fixtures are special functions that pytest uses for setup and cleanup.
+Any test case can call fixtures, making them very reusable. 
+Fixtures are special functions that pytest can call before test case functions. 
+They're the best way to handle "Arrange" steps shared by multiple tests in the context of the Arrange-Act-Assert pattern.
+'''
+#test_accumulator_init() verifies that the new instance of the Accumulator class has a starting count of zero.
+
+@pytest.fixture
+def accum():
+    return Accumulator()
+def test_accumulator_init(accum):
+    assert accum.count == 0
+
+#test_accumulator_add_one() verifies that the add() method adds one to the internal count when it is called with no other arguments.
+def test_accumulator_add_one(accum):
+    accum.add()
+    assert accum.count == 1
+
+#test_accumulator_add_two() verifies that the count increases appropriately with multiple add() calls.
+def test_accumulator_add_two(accum):
+    accum.add()
+    accum.add()
+    assert accum.count == 2
+#test_accumulator_add_three() verifies that the add() method adds 3 to the count when it is called with the argument of 3.
+def test_accumulator_add_three(accum):
+    accum = Accumulator()
+    accum.add(3)
+    assert accum.count == 3
+#Finally, test_accumulator_cannot_set_count_directly() verifies that the count attribute cannot be assigned directly because it is a read-only property.
+# Notice how we use pytest.raises to verify the AttributeError.
+def test_accumulator_cannot_set_count_directly(accum):
+  accum = Accumulator()
+  with pytest.raises(AttributeError, match=r"property 'count' of 'Accumulator' object has no setter") as e:
+    accum.count = 10
+
+
+'''
+How does the fixture work? It's pytest magic! 
+When pytest discovers a test case function, it looks at the function's parameter list. 
+If the function has parameters, then it will search for fixtures to match each parameter's name.
+
+In our case, the test function has a parameter named accum, so pytest looks for a fixture named accum which it will find in the same module. 
+pytest will then execute the fixture and pass the fixture's return value into the test case function. 
+Thus, in our test case, the accum variable will refer to the new Accumulator object created by the accum fixture. Nifty.
+
+This is a clever form of dependency injection. The test case doesn't set up or "arrange" the test objects itself. 
+Instead, the fixture handles setup and injects the required objects as dependencies into the test function. 
+This separation of concerns makes test cases more readable, more consistent, and more maintainable. 
+It also makes new test cases easier to write.
+'''
